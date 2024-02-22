ef43268949872f98da03314a7a039f544e8d8191
Calyen93 <genco.claudio3@gmail.com>
2024-02-22 16:56:20 +0100

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//Scrivere un programma che contenga una classe astratta chiamata
+//Forma ed un metodo astratto chiamato calcolaArea().
+//Crea poi due sottoclassi Rettangolo e Triangolo che estendono
+//Forma ed implementano il metodo per il calcolo dell'area in maniera specifica.
+
+public class Main {
+    public static void main(String[] args) {
+
+        Rectangle rectangle = new Rectangle(5, 10);
+        double areaRectangle = rectangle.calculateArea();
+        System.out.println("Area del Rettangolo: " + areaRectangle);
+
+        Triangle triangle = new Triangle(3, 6);
+        double areaTriangle = triangle.calculateArea();
+        System.out.println("Area del triangolo: "  + areaTriangle);
+    }
+}
